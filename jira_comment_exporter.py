dc6ea2e6f16e4ab7ff629849026f84c9d47d8f14
Plourde <jean-michel.plourde@tlmgo.com>
2025-02-09 20:43:17 -0500

    Bullet trace comment parsing

--- jira_comment_exporter.py
+++ jira_comment_exporter.py
@@ -1,7 +1,61 @@
-import requests
-import json
 import os
-from datetime import datetime
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
-JIRA_BASE_URL = "https://mrdltd.atlassiant.net"
-JIRA_API_TOKEN = os.environ["JIRA_API_TOKEN"]
+from dotenv import load_dotenv
+
+load_dotenv()
+
+JIRA_BASE_URL = "https://mrdltd.atlassian.net"
+JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
+JIRA_EMAIL = "jean-michel.plourde@tlmgo.com"
+
+
+def get_ticket_comments(ticket_key):
+    url = f"{JIRA_BASE_URL}/rest/api/3/issue/{ticket_key}/comment"
+    auth = HTTPBasicAuth(JIRA_EMAIL, JIRA_API_TOKEN)
+    headers = {
+        "Accept": "application/json"
+    }
+
+    response = requests.request(
+        "GET",
+        url,
+        headers=headers,
+        auth=auth
+    )
+
+    return json.loads(response.text)
+
+
+def main():
+    response = get_ticket_comments("CLD-223")
+    comments = response["comments"]
+
+    for comment_idx, comment in enumerate(comments):
+        print("\n", "comment:", comment_idx, "----------------")
+        content = comment["body"]["content"]
+        for part_idx, part in enumerate(content):
+            if part["type"] == "paragraph":
+                paragraph = ""
+                if part_idx != 0:
+                    paragraph += "\n"
+                for element in part["content"]:
+                    if element["type"] == "text":
+                        text = element["text"]
+                        marks = element.get("marks", [])
+                        for mark in marks:
+                            if mark["type"] == "code":
+                                text = f"`{text}`"
+
+                        paragraph += text
+
+                    elif element["type"] == "hardBreak":
+                        paragraph += "\n"
+
+                print(paragraph)
+
+
+if __name__ == "__main__":
+    main()
